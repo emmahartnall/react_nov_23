81088b2c3e92c80fccfb424cb30dd0f3a7b4bfdb
Emma Hartnall | Hall <emmahartnall@gmail.com>
2024-12-02 13:43:16 +1100

    b

--- src/App.js
+++ src/App.js
@@ -3,17 +3,32 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>Emma's quoting app</h1>
-      <MyButton />
-      
+      <Header/>
+      <main>
+        <h1>Emma's quoting app</h1>
+        <MyButton />
+      </main>
     </div>
   );
 }
 
 function MyButton() {
   return (
-    <button>I'm a button</button>
+    <button>Get Quote</button>
   );
 }
+
+const Header = () => {
+  return (
+    <header className="app-header">
+      <div className="logo">Quoting App</div>
+      <nav className="nav-links">
+        <a href="#home">Home</a>
+        <a href="#quotes">Quotes</a>
+        <a href="#contact">Contact</a>
+      </nav>
+    </header>
+  );
+};
 
 export default App;
