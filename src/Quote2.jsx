07b6387891a21c7c2e15b7422012a6f6ed5537b1
Emma Hartnall | Hall <emmahartnall@gmail.com>
2025-01-14 19:37:58 +1100

    added desciption

--- src/Quote2.jsx
+++ src/Quote2.jsx
@@ -1,7 +1,8 @@
 // Quote2.jsx
 import React, { useState } from "react";
-//import Heading from "./components/Heading";
 import ProjectDetailsForm from "./components/ProjectDetailsForm";
+import Heading from "./components/Heading";
+import DescriptionMethodology from "./components/DescriptionMethodology";
 import FeatureSelection from "./components/FeatureSelection";
 import CostSummary from "./components/CostSummary";
 import Header from "./components/Header";
@@ -36,7 +37,16 @@ const Quote = () => {
       <main>=
         <section class="transparent-section">
           <div class="content">
-           
+            <Heading>Price Estimation Tool for your Web Application</Heading> 
+
+            <DescriptionMethodology>
+              <p>We use an iterative approach to the development of our web applications and as such we only provide fixed quotes on small pieces of software at a time.</p>
+              <p>Our methodology is based on the principle that customers gain insights into the requirements after interacting with working software.</p>
+              <p>Using this approach maximises flexiblility and reduces risk for the customer.</p>
+
+              <p>Also we only charge on delivery of working software.</p>
+              <p>This tool is just to provide a guide towards costs.</p> 
+            </DescriptionMethodology>
             <ProjectDetailsForm setProjectDetails={setProjectDetails} />
             <FeatureSelection
               selectedFeatures={selectedFeatures}
@@ -48,7 +58,7 @@ const Quote = () => {
               totalCost={calculateTotalCost()}
             />
           </div>
-          </section>
+        </section>
       
       </main>
       
--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,16 +1,11 @@
-// components/Heading.jsx
 import React from "react";
 
-const Heading = () => {
+function Heading({ children }) {
   return (
-    <Heading>
-      <h1>Price Estimation Tool Web Application development.</h1>
-      <p>We use an iterative method to develop web applications, so we only provide fixed estimates on small pieces of work at a time.</p>
-      <p>You also only pay for working software.</p>
-      <p>This tool is just to provide a guide towards costs.</p>
-    </Heading>
-    
+    <h1>
+      {children}
+    </h1>
   );
-};
+}
 
 export default Heading;
